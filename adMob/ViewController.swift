389a467664913d313920fab0a978eb14271e998c
msnbek <mahmutsenbek@icloud.com>
2023-10-11 13:55:45 +0300

    Initial Commit

---
+++ adMob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adMob
+//
+//  Created by Mahmut Senbek on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
